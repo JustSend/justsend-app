20bd37e50064a764c7ec9e09fe073cade9c450fe
husseymarcos <mhussey@mail.austral.edu.ar>
2025-05-28 17:23:23 -0300

    feat: implement user registration with API integration and add useBalance hook

--- lib/api.ts
+++ lib/api.ts
@@ -1,38 +1,26 @@
-import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
-import Cookies from 'js-cookie';
+import axios, { InternalAxiosRequestConfig } from 'axios';
+import { auth } from '@/firebaseConfig';
 
 const BACKEND_URL = process.env.EXPO_PUBLIC_API_URL || 'put variable in .env';
 
-export const api = axios.create({
+export const apiPublic = axios.create({
   baseURL: BACKEND_URL,
-  withCredentials: true,
 });
 
-api.interceptors.request.use(
-  (config: InternalAxiosRequestConfig) => {
-    const token = Cookies.get('token');
+export const apiPrivate = axios.create({
+  baseURL: BACKEND_URL,
+});
 
-    if (token && config.headers) {
-      config.headers.Authorization = `Bearer ${token}`;
+apiPrivate.interceptors.request.use(
+  async (config: InternalAxiosRequestConfig) => {
+    const user = auth.currentUser;
+    if (user) {
+      const token = await user.getIdToken();
+      if (token && config.headers) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
     }
-
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-api.interceptors.response.use(
-  (response) => response,
-  (error: AxiosError) => {
-    if (
-      error.response &&
-      (error.response.status === 401 || error.response.status === 403)
-    ) {
-      Cookies.remove('token');
-      window.location.href = '/login';
-    }
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
