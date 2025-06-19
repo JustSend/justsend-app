969e410a0c76f49af5b5568810398e2d8e68ad7f
lucasmonteverdi1 <128722688+lucasmonteverdi1@users.noreply.github.com>
2025-06-19 12:18:36 -0300

    cypress: add register test cases and .env.sample for test directory

---
+++ test/cypress/e2e/register.cy.ts
@@ -0,0 +1,45 @@
+describe('Register user', () => {
+  const email: string = Cypress.env('TEST_USER_EMAIL');
+  const password: string = Cypress.env('TEST_USER_PASSWORD');
+
+  it('should load the register form', () => {
+    cy.visit('/register');
+    cy.get('[data-testid="Email Input"]').should('exist');
+    cy.get('[data-testid="Password Input"]').should('exist');
+    cy.get('[data-testid="Register Button"]').should('exist');
+    cy.get('[data-testid="Sign In Button"]').should('exist');
+  });
+
+  /* Change the credentials after running the test. */
+  it('should register a new user successfully', () => {
+    cy.visit('/register');
+    cy.get('[data-testid="Email Input"]').type(email);
+    cy.get('[data-testid="Password Input"]').type(password);
+    cy.get('[data-testid="Register Button"]').click();
+    cy.get('[data-testid="Error Message"]').should('not.exist');
+  });
+
+  it('registering user with used email should fail', () => {
+    cy.visit('/register');
+    cy.get('[data-testid="Email Input"]').type(email);
+    cy.get('[data-testid="Password Input"]').type(password);
+    cy.get('[data-testid="Register Button"]').click();
+    cy.get('[data-testid="Error Message"]').should('exist');
+  });
+
+  it('registering with an empty email should fail', () => {
+    cy.visit('/register');
+    cy.get('[data-testid="Email Input"]').type('a');
+    cy.get('[data-testid="Password Input"]').type(password);
+    cy.get('[data-testid="Register Button"]').click();
+    cy.get('[data-testid="Error Message"]').should('exist');
+  });
+
+  it('registering with a weak password should fail', () => {
+    cy.visit('/register');
+    cy.get('[data-testid="Email Input"]').type(email);
+    cy.get('[data-testid="Password Input"]').type('abc');
+    cy.get('[data-testid="Register Button"]').click();
+    cy.get('[data-testid="Error Message"]').should('exist');
+  });
+});
